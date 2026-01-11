76cffe824b8a1fee52478ce68a22f6d8bca0440a
havor13 <sampsonhavor43@gmail.com>
2026-01-11 21:56:30 +0000

    Initial commit for CSE Motors Assignment 1

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`CSE Motors running at http://localhost:${PORT}`);
+});
